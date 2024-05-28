4c21c642ef128717d2b69828e0ff92036f986b67
Henrik Liljestrand <henrik.liljestrand@hotmail.com>
2024-05-28 13:43:17 +0200

    Added ICustomerService and implemented it in CustomerService

---
+++ CosmosMinimalApi.Api/Core/Interfaces/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using CosmosMinimalApi.Api.Domain;
+
+namespace CosmosMinimalApi.Api.Core.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task AddNewCustomer(Customer customer);
+        Task<bool> UpdateCustomer(Customer customer);
+        Task<bool> DeleteCustomer(Customer customer);
+        Task<List<Customer>> GetCuustomersByName(string searchVal);
+        Task<List<Customer>> GetCustomersBySalesPersonName(string searchVal);
+    }
+}
---
+++ CosmosMinimalApi.Api/Core/Services/CustomerService.cs
@@ -0,0 +1,33 @@
+﻿using CosmosMinimalApi.Api.Core.Interfaces;
+using CosmosMinimalApi.Api.Domain;
+
+namespace CosmosMinimalApi.Api.Core.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        public Task AddNewCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Customer>> GetCustomersBySalesPersonName(string searchVal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Customer>> GetCuustomersByName(string searchVal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
